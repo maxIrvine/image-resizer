679e9e8618ea007a51ca7f147d70df8fc9b1fffa
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-08-03 13:40:01 -0400

    adds git ignore

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const fs = require('fs');
+
+var request = require('request');
+request('http://www.google.com', function (error, response, body) {
+  console.log('error:', error); // Print the error if one occurred 
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+  console.log('body:', body); // Print the HTML for the Google homepage. 
+});
